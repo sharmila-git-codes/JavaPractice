e4d44a43e4c60ae955c0c120a431b7a548b948e5
sharmila <sharmila@macbookpro.lan>
2025-11-19 11:20:17 +0530

    refs #Javapractice initial commit

---
+++ FirstProject/src/com/java/day1/Variables.java
@@ -0,0 +1,24 @@
+package com.java.day1;
+
+class Variables {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("****WELCOME TO JAVA CLASS****");
+		
+		int age = 23;
+		String firstName = "Sharmila";
+		System.out.println("Age"+age+"FirstName"+firstName);
+		
+		System.out.println(firstName+" is a java trainer and his age is "+age);
+		
+//		Identifiers
+		String _printIdentifer = "Hellooo";
+//		String char = "doctor";
+		String character = "doctor";
+		
+		
+		
+	}
+
+}
