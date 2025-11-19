04b6b46387f67430a8fe6bbb09328714f8ccb0a0
Sharmila <sharmila@MacBookPro.lan>
2025-11-19 13:02:52 +0530

    refs #assignmentone - 19th Nov

---
+++ FirstProject/src/com/java/day1/AssignmentOne.java
@@ -0,0 +1,20 @@
+package com.java.day1;
+
+public class AssignmentOne {
+
+	public static void main(String[] args) {
+
+		System.out.println("****WAJP to print Tom is Jerry friend and his age is 20.****");
+
+		String firstName = "Tom";
+		String friendName = "Jerry";
+		int friendAge = 20;
+		System.out.println(firstName + " is " + friendName + " friend and his age is " + friendAge);
+
+		System.out.println("****WAJP to demonstrate Datatypes****");
+		byte byteDataType= 1;
+		
+	}
+	
+
+}
